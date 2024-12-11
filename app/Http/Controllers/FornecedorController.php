40c3d9e191a5202ae416eb7fae4c3b61e3531150
Leandro Debian <leandro_ccb10@hotmail.com>
2024-12-11 18:20:05 -0300

    Criando os arquivos de modelos, controllers, migrations, factories, seeders, request das entidades

---
+++ app/Http/Controllers/FornecedorController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreFornecedorRequest;
+use App\Http\Requests\UpdateFornecedorRequest;
+use App\Models\Fornecedor;
+
+class FornecedorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFornecedorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Fornecedor $fornecedor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Fornecedor $fornecedor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFornecedorRequest $request, Fornecedor $fornecedor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Fornecedor $fornecedor)
+    {
+        //
+    }
+}
