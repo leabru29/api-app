ca26b4a3f2bf5e08ca9427d3a15292ecbaf172fa
Leandro Debian <leandro_ccb10@hotmail.com>
2024-12-12 11:52:45 -0300

    Adicionado os relacionamentos no modelo

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Categoria extends Model
 {
@@ -11,5 +13,15 @@ class Categoria extends Model
 
     protected $table = 'categorias';
 
-    protected $fillable = ['nome'];
+    protected $fillable = ['nome', 'fornecedor_id'];
+
+    public function fornecedor(): BelongsTo
+    {
+        return $this->belongsTo(Fornecedor::class);
+    }
+
+    public function produtos(): HasMany
+    {
+        return $this->hasMany(Produto::class);
+    }
 }
